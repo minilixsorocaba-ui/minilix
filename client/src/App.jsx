d5de09da099b4dca944ad029cd5ce33e4ae003a9
minilixsorocaba-ui <minilix.sorocaba@gmail.com>
2026-09-15 19:42:40 -0300

    feat: integrar relatorios gerenciais V5.4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import App from './AppPricingRules.jsx';
 import V5Dashboard from './V5Dashboard.jsx';
 import V5Clients from './V5Clients.jsx';
 import V5Assets from './V5Assets.jsx';
+import V54Reports from './V54Reports.jsx';
 
 export default function AppV5(){
-  return <><App/><V5Dashboard/><V5Clients/><V5Assets/></>;
+  return <><App/><V5Dashboard/><V5Clients/><V5Assets/><V54Reports/></>;
 }
