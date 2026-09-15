5b3274bdce67bea7d48ff6e92b3823c836139589
minilixsorocaba-ui <minilix.sorocaba@gmail.com>
2026-09-15 19:38:25 -0300

    feat: integrar clientes e historico V5.2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import App from './AppPricingRules.jsx';
 import V5Dashboard from './V5Dashboard.jsx';
+import V5Clients from './V5Clients.jsx';
 
 export default function AppV5(){
-  return <><App/><V5Dashboard/></>;
+  return <><App/><V5Dashboard/><V5Clients/></>;
 }
