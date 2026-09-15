abd02815e81afa877f10da9c1b6cc975d3ffa088
minilixsorocaba-ui <minilix.sorocaba@gmail.com>
2026-09-15 19:36:16 -0300

    feat: integrar central gerencial V5

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,2 +1,6 @@
 import App from './AppPricingRules.jsx';
-export default App;
+import V5Dashboard from './V5Dashboard.jsx';
+
+export default function AppV5(){
+  return <><App/><V5Dashboard/></>;
+}
