ecd82665c8976ec47008500e5f7703f6e0402f61
minilixsorocaba-ui <minilix.sorocaba@gmail.com>
2026-09-16 09:02:14 -0300

    feat: integrar painel operacional V5.5

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,8 @@ import V5Dashboard from './V5Dashboard.jsx';
 import V5Clients from './V5Clients.jsx';
 import V5Assets from './V5Assets.jsx';
 import V54Reports from './V54Reports.jsx';
+import V55Operational from './V55Operational.jsx';
 
 export default function AppV5(){
-  return <><App/><V5Dashboard/><V5Clients/><V5Assets/><V54Reports/></>;
+  return <><App/><V5Dashboard/><V5Clients/><V5Assets/><V54Reports/><V55Operational/></>;
 }
