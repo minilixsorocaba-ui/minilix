134b64ae3525d239c817c6f0577e2628a38ec65c
minilixsorocaba-ui <minilix.sorocaba@gmail.com>
2026-09-18 21:01:40 -0300

    feat: integrar central de solicitacoes ao painel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ import V5Clients from './V5Clients.jsx';
 import V5Assets from './V5Assets.jsx';
 import V54Reports from './V54Reports.jsx';
 import V55Operational from './V55Operational.jsx';
+import V56ClientRequests from './V56ClientRequests.jsx';
 
 export default function AppV5(){
-  return <><App/><V5Dashboard/><V5Clients/><V5Assets/><V54Reports/><V55Operational/></>;
+  return <><App/><V5Dashboard/><V5Clients/><V5Assets/><V54Reports/><V55Operational/><V56ClientRequests/></>;
 }
