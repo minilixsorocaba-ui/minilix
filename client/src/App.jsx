39f44143152bfa8675fb5b2f7d0320db8319a556
minilixsorocaba-ui <minilix.sorocaba@gmail.com>
2026-09-18 21:41:54 -0300

    feat: integrar central de pedidos V6.2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,8 @@ import V55Operational from './V55Operational.jsx';
 import V56ClientRequests from './V56ClientRequests.jsx';
 import V6Units from './V6Units.jsx';
 import V61Clients from './V61Clients.jsx';
+import V62Pedidos from './V62Pedidos.jsx';
 
 export default function AppV5(){
-  return <><App/><V5Dashboard/><V5Clients/><V5Assets/><V54Reports/><V55Operational/><V56ClientRequests/><V6Units/><V61Clients/></>;
+  return <><App/><V5Dashboard/><V5Clients/><V5Assets/><V54Reports/><V55Operational/><V56ClientRequests/><V6Units/><V61Clients/><V62Pedidos/></>;
 }
