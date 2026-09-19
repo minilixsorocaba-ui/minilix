ba87ca09068d50dd2c1d69de5074f24f217329ab
minilixsorocaba-ui <minilix.sorocaba@gmail.com>
2026-09-18 21:09:29 -0300

    feat: integrar modulo V6 de unidades ao painel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,8 @@ import V5Assets from './V5Assets.jsx';
 import V54Reports from './V54Reports.jsx';
 import V55Operational from './V55Operational.jsx';
 import V56ClientRequests from './V56ClientRequests.jsx';
+import V6Units from './V6Units.jsx';
 
 export default function AppV5(){
-  return <><App/><V5Dashboard/><V5Clients/><V5Assets/><V54Reports/><V55Operational/><V56ClientRequests/></>;
+  return <><App/><V5Dashboard/><V5Clients/><V5Assets/><V54Reports/><V55Operational/><V56ClientRequests/><V6Units/></>;
 }
