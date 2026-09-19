c22eda4ea5a4a6d36e653ef22f1107a8e7c1e200
minilixsorocaba-ui <minilix.sorocaba@gmail.com>
2026-09-18 21:33:50 -0300

    feat: integrar central de clientes V6.1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,8 @@ import V54Reports from './V54Reports.jsx';
 import V55Operational from './V55Operational.jsx';
 import V56ClientRequests from './V56ClientRequests.jsx';
 import V6Units from './V6Units.jsx';
+import V61Clients from './V61Clients.jsx';
 
 export default function AppV5(){
-  return <><App/><V5Dashboard/><V5Clients/><V5Assets/><V54Reports/><V55Operational/><V56ClientRequests/><V6Units/></>;
+  return <><App/><V5Dashboard/><V5Clients/><V5Assets/><V54Reports/><V55Operational/><V56ClientRequests/><V6Units/><V61Clients/></>;
 }
